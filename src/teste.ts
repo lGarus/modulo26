63cd4bd9f88c32f06e726853cdec09c9f254cc9f
lGarus <128258828+lGarus@users.noreply.github.com>
2023-05-23 16:09:04 -0300

    Add files via upload

---
+++ src/teste.ts
@@ -0,0 +1,11 @@
+function calculaArea(b: number, a: number): number {
+    return a * b
+}
+
+console.log(calculaArea(7, 5))
+
+const nome = "Guilherme"
+
+const apresentacao: string = `Olá ${nome}`
+
+console.log(apresentacao)
